a3f5ee74da1d5a41bb1ed04a1bd630b5ff8dcee7
George Theofilis <theofilis.g@gmail.com>
2019-04-10 15:53:11 +0300

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -56,6 +56,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.com/chariot-h2020/chariot-health-service',
-    version='version='0.1.0'',
+    version='version='0.1.1'',
     zip_safe=False,
 )
