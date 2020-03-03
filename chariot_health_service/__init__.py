bdfd08096c4998c0a1e18a4992021d7ce40e0c8c
George Theofilis <theofilis.g@gmail.com>
2020-03-03 10:18:14 +0000

    Bump version: 0.1.3 → 0.11.0

--- chariot_health_service/__init__.py
+++ chariot_health_service/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '0.1.3'
+__version__ = '0.11.0'
 
 __service_name__ = f'health-http-service-{__version__}'
 __service_name_listener__ = f'health-listener-service-{__version__}'
